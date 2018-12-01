496bdacbafb6b846f47827bc52bb62ecf00de1fe
dingshuaiyang <dingshuaiyang@itheima.com>
2018-12-01 20:48:50 +0800

    修改后提交推送

--- src/main/java/com/itheima/controller/Bach.java
+++ src/main/java/com/itheima/controller/Bach.java
@@ -4,8 +4,7 @@ public class Bach {
     //测试
     public static void main(String[] args) {
         System.out.println("lllllllll");
-    }
-    public static void main(String[] args) {
+
         System.out.println("rrtytrtytr");
     }
 }
